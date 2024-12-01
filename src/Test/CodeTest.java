0eba0fdf3fa33df418d93b4d504ccf00a883ab03
this-yujunkim <yujunkim2004@gmail.com>
2024-12-02 01:29:44 +0900

    FEAT: add mockito into project
    also edited `CodeTest.java` using mockito.

--- src/Test/CodeTest.java
+++ src/Test/CodeTest.java
@@ -1,10 +1,14 @@
+import entity.EnemyShip;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class CodeTest {
 
     @Test
-    void destroy() {
-        assertTrue(true);
+    void test() {
+        EnemyShip e = mock(EnemyShip.class);
+        when(e.getX()).thenReturn(10);
+        assertEquals(10, e.getX());
     }
 }
